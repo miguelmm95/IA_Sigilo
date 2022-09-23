5ff73986e42fd2a94097e33bf279af3810272bbc
miguelmm95 <79021700+miguelmm95@users.noreply.github.com>
2022-09-23 22:59:37 +0200

    wallDetection_basic

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public bool isMoving;
+    public float movementSpeed = 150f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        isMoving = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isMoving)
+        {
+            //rb.AddForce(movementSpeed * Time.deltaTime, 0, 0);
+            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/wallDetection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallDetection : MonoBehaviour
+{
+
+    RaycastHit hit;
+    public movement p_movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p_movement = GameObject.FindObjectOfType<movement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, Vector3.right);
+        Debug.DrawRay(transform.position, Vector3.right, Color.red);
+
+        if(Physics.Raycast(ray, out hit, 1f))
+        {
+            if(hit.collider.tag == "pared")
+            {
+                p_movement.isMoving = false;
+            }
+        }
+    }
+}
